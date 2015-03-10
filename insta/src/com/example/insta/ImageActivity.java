70a5bbae1047881a34ab0ec1576d556c1f6bf828
sreeharshanv <sreeharshanv@gmail.com>
2015-03-09 22:31:50 -0500

    Initial commit

---
+++ insta/src/com/example/insta/ImageActivity.java
@@ -0,0 +1,75 @@
+package com.example.insta;
+import android.app.AliasActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.sample.csgallery.network.BitmapDownloaderTask;
+
+
+public class ImageActivity extends AliasActivity {
+    ZoomImage zoomImage;
+    Button back;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+        Intent i = getIntent();
+        String url = i.getExtras().getString("url");
+
+        if (url.length() > 0) {
+
+            zoomImage = (ZoomImage) findViewById(R.id.zoom_image);
+
+            // start a task to download the image
+            BitmapDownloaderTask task = new BitmapDownloaderTask(zoomImage);
+            if (!task.searchCache(url))
+                task.execute(url);
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "Error Loading photo", Toast.LENGTH_SHORT).show();
+        }
+        back=(Button)findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private Intent getIntent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_image, menu);
+        return true;
+    }
+
+    private Object getMenuInflater() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ insta/src/com/example/insta/ZoomImage.java
@@ -0,0 +1,97 @@
+package com.example.insta;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.ScaleGestureDetector;
+import android.widget.ImageView;
+
+public class ZoomImage extends ImageView
+{
+    //stores the size of the screen along with its smallest dimension
+    private int iWidth,iHeight;
+    private int width,height;
+
+    //track pinching points
+    private float scale;
+    private static float minZoom=0.6f,maxZoom=4f;
+    private Matrix matrix;
+    private ScaleGestureDetector scaleGestureDetector;
+
+
+    public ZoomImage(Context context) {
+        super(context);
+    }
+
+    public ZoomImage(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ZoomImage(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void setImageBitmap(Bitmap bm)
+    {
+        super.setImageBitmap(bm);
+
+        iWidth = bm.getWidth();
+        iHeight = bm.getHeight();
+
+        setupPinchZoom();
+
+        Log.i("crb", "image size = (" + iWidth + ", " + iHeight + ")");
+    }
+    public void setupPinchZoom()
+    {
+        DisplayMetrics metrics = getContext().getResources().getDisplayMetrics();
+        width = metrics.widthPixels;
+        height = metrics.heightPixels;
+
+        // set the size of the picture to fit the smallest orientation of the screen
+        if (width > height)
+        {
+            scale = (float) height / iHeight;
+        }
+        else
+        {
+            scale = (float) width / iWidth;
+        }
+
+        // adjust scaling limits if needed
+        if (scale > maxZoom)
+            maxZoom = scale;
+        if (scale < minZoom)
+            minZoom = scale;
+
+        // create a transformation matrix and set the scale and translation based on the initial scale
+        matrix = new Matrix();
+        matrix.setScale(scale, scale);
+        matrix.postTranslate(-306 * scale, -306 * scale);
+
+        // set up a listener for pinch and zoom
+        scaleGestureDetector = new ScaleGestureDetector(getContext(), new GuestureListener());
+    }
+    private class GuestureListener extends ScaleGestureDetector.SimpleOnScaleGestureListener
+    {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector)
+        {
+            // adjust our scaling based on the gesture
+            scale *= detector.getScaleFactor();
+            scale = Math.max(minZoom, Math.min(scale, maxZoom));
+
+            // adjust matrix
+            matrix.setScale(scale, scale);
+            matrix.postTranslate(-306 * scale, -306 * scale);
+
+            // tell the canvas to redraw
+            invalidate();
+            return true;
+        }
+    }
+}
